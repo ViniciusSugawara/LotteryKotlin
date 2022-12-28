30049e2d45fe4355708ddf9b71c315a95bd67ede
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-27 22:40:46 -0300

    feat: Main code is now working.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,44 @@
+import models.Game
 import models.Player
 import models.Ticket
+import models.Randomizer
 
 fun main() {
     println("Sistema de loteria, adaptado para Kotlin.")
-    val testTicket = Ticket()
-    for(i in testTicket.numbers){
+
+    val game = Game(10, 2, 6.0)
+    val ticketRandomizer = Randomizer(game.maxNumber, game.limitOfNumbersDrawn)
+    val testTicket = Ticket(ticketRandomizer.randomizedNumbers)
+    val anotherTicket = Ticket(ticketRandomizer.randomizedNumbers)
+
+    println("First ticket: Numbers are")
+    testTicket.numbers.forEach{
+        println(it)
+    }
+
+    println("Second ticket: Numbers are")
+    for(i in anotherTicket.numbers){
         println(i)
     }
-    val allTickets = Array<Ticket>(5) { testTicket}
+
+    val allTickets: MutableSet<Ticket> = mutableSetOf(testTicket, anotherTicket)
     val playerOne = Player("Josh", allTickets)
+
     println("The following player has been added: ${playerOne.name}")
+
+    val mainRandomizer = Randomizer(game.maxNumber, game.limitOfNumbersDrawn)
+
+    val premiumTicket = Ticket(mainRandomizer.randomizedNumbers)
+    premiumTicket.numbers.forEach{
+
+        println("Numbers drawn were " + it)
+    }
+
+    for(ticket in allTickets){
+        when(ticket.equals(premiumTicket)){
+            true -> println("Has won")
+            else -> println("Hasn't won")
+        }
+    }
+
 }
